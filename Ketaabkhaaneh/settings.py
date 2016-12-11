b9ac6b18d90f66454a742d5b1a54ea054df78abb
navid azami <navid.azami@gmail.com>
2016-12-11 23:44:51 +0330

    homepage.

--- Ketaabkhaaneh/settings.py
+++ Ketaabkhaaneh/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Ketaabkhaaneh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,  'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +66,11 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_URL = '/static/'
 
 WSGI_APPLICATION = 'Ketaabkhaaneh.wsgi.application'
 
@@ -119,7 +124,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
-    'bookreview',
-)
+from local_settings import *
